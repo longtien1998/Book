27998cf7476f32c41c52fb4e2abd44a5e8fc3054
HaiDeng03 <haidangngo61@gmail.com>
2024-11-20 01:42:56 +0700

    dng

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BooksController;
+use App\Http\Controllers\Api\CategoriesController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,4 +18,9 @@ Route::prefix('books')->group(function () {
     //Lấy sách theo thể loại
     Route::get('/category/{id}', [BooksController::class, 'filterByCategory']);
 });
-
+Route::prefix('categories')->group(function () {
+    //Lấy tất cả thể loại
+    Route::get('/', [CategoriesController::class, 'index']);
+    //Lấy thể loại theo id
+    Route::get('/{id}', [CategoriesController::class, 'show']);
+});
